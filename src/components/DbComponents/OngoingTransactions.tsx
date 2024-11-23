a2c773b301ba7a7e3664cd2258fe0ebdf2203998
PhilReact <philliplangmartinez@gmail.com>
2024-11-23 13:34:46 +0200

    change logo

--- src/components/DbComponents/OngoingTransactions.tsx
+++ src/components/DbComponents/OngoingTransactions.tsx
@@ -5,7 +5,7 @@ import { useIndexedDBContext } from "../../contexts/indexedDBContext";
 
 const fetchTradeInfo = async (qortalAtAddress) => {
 
-  const checkIfOfferingRes = await fetch(`http://127.0.0.1:12391/crosschain/trade/${qortalAtAddress}`)
+  const checkIfOfferingRes = await fetch(`/crosschain/trade/${qortalAtAddress}`)
   const data = await checkIfOfferingRes.json()
    return data
 };
